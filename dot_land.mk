5fd1afe1a152398923a06eb92580381de86907cd
AdityaKevin <adityakevin50@gmail.com>
2019-02-16 06:47:30 +0000

    dot

--- dot_land.mk
+++ dot_land.mk
@@ -22,10 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
-
-TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from land device
@@ -34,7 +32,7 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_land
+PRODUCT_NAME := dot_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
